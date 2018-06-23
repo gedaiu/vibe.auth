8256d9f7060255d6392a85c276413746ba039378
Szabo Bogdan <szabobogdan@yahoo.com>
2018-06-23 11:18:31 +0200

    test the resource handler

--- source/vibeauth/router/resources/routes.d
+++ source/vibeauth/router/resources/routes.d
@@ -5,6 +5,7 @@ import std.net.curl;
 import std.file;
 import std.path;
 import std.digest.md;
+import std.algorithm;
 import std.algorithm.mutation : copy;
 
 import core.time;
@@ -14,6 +15,13 @@ import vibe.core.file;
 import vibe.http.server;
 import vibe.stream.operations;
 import vibeauth.configuration;
+
+version(unittest) {
+  import vibe.http.router;
+  import fluentasserts.vibe.request;
+  import fluentasserts.vibe.json;
+  import fluent.asserts;
+}
 
 enum bootstrapStyleUrl = "https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css";
 enum localBootstrapStyle = "tmp/assets/bootstrap.min.css";
@@ -40,24 +48,102 @@ shared static this() {
     mkdirRecurse(localJquery.dirName);
     download(jqueryUrl, localJquery);
   }
+
+  version(unittest) {
+    std.file.write("tmp/test.css", "data");
+    std.file.write("tmp/test.js", "data");
+  }
 }
 
+/// Static resource
 struct Resource(string path) {
   static immutable {
     string etag;
     string data;
+    string mime;
   }
 
+  ///
   static this() {
     auto ctx = makeDigest!MD5();
 
     data = readText(path);
     copy(data, &ctx); //Note: You must pass a pointer to copy!
     etag = ctx.finish().toHexString.idup;
+
+    if(path.endsWith(".css")) {
+      mime = "text/css";
+    }
+
+    if(path.endsWith(".js")) {
+      mime = "text/javascript";
+    }
+  }
+
+  ///
+  void handler(HTTPServerRequest req, HTTPServerResponse res) {
+    res.headers["Cache-Control"] = "max-age=86400";
+    res.headers["ETag"] = etag;
+
+    if("If-None-Match" in req.headers && req.headers["If-None-Match"] == etag) {
+      res.statusCode = 304;
+      res.writeVoidBody;
+      return;
+    }
+
+    res.statusCode = 200;
+    res.writeBody(data, mime);
   }
 }
 
-/// Handle the registration routes
+/// It should store the the valid properties
+unittest {
+  Resource!"tmp/test.css" resource;
+  resource.data.should.equal("data");
+  resource.mime.should.equal("text/css");
+  resource.etag.should.equal("8D777F385D3DFEC8815D20F7496026DC");
+
+  Resource!"tmp/test.js" resourceJs;
+  resourceJs.mime.should.equal("text/javascript");
+}
+
+/// It should get a resource with the right headers
+unittest {
+  Resource!"tmp/test.css" resource;
+
+  auto router = new URLRouter();
+  router.get("/resource", &resource.handler);
+
+  router
+    .request
+    .get("/resource")
+    .expectHeader("ETag", resource.etag)
+    .expectHeader("Content-Type", "text/css")
+    .expectStatusCode(200)
+    .end((Response response) => {
+      response.bodyString.should.equal("data");
+    });
+}
+
+/// It should not get a resource with the etag matches
+unittest {
+  Resource!"tmp/test.css" resource;
+
+  auto router = new URLRouter();
+  router.get("/resource", &resource.handler);
+
+  router
+    .request
+    .get("/resource")
+    .header("If-None-Match", resource.etag)
+    .expectHeader("ETag", resource.etag)
+    .expectStatusCode(304)
+    .end((Response response) => {
+      response.bodyString.should.equal("");
+    });
+}
+
+/// Handle resources served by vibe auth
 class ResourceRoutes {
 
   private {
@@ -65,11 +151,9 @@ class ResourceRoutes {
       ServiceConfiguration configuration;
     }
 
-    immutable {
-      Resource!localBootstrapStyle _bootstrapStyle;
-      Resource!localBootstrapJs _bootstrapJs;
-      Resource!localJquery _jquery;
-    }
+    Resource!localBootstrapStyle _bootstrapStyle;
+    Resource!localBootstrapJs _bootstrapJs;
+    Resource!localJquery _jquery;
   }
 
   ///
@@ -83,52 +167,16 @@ class ResourceRoutes {
   /// Handle the requests
   void handler(HTTPServerRequest req, HTTPServerResponse res) {
     if(req.method == HTTPMethod.GET && req.path == configuration.paths.resources.bootstrapStyle) {
-      bootstrapStyle(req, res);
+      _bootstrapStyle.handler(req, res);
     }
 
     if(req.method == HTTPMethod.GET && req.path == configuration.paths.resources.bootstrapJs) {
-      bootstrapJs(req, res);
+      _bootstrapJs.handler(req, res);
     }
 
     if(req.method == HTTPMethod.GET && req.path == configuration.paths.resources.jquery) {
-      jquery(req, res);
+      _jquery.handler(req, res);
     }
-  }
-
-  void bootstrapStyle(HTTPServerRequest req, HTTPServerResponse response) {
-    if("If-None-Match" in req.headers && req.headers["If-None-Match"] == _bootstrapStyle.etag) {
-      response.statusCode = 304;
-      response.writeVoidBody;
-      return;
-    }
-
-    response.headers["Cache-Control"] = "max-age=3600";
-    response.headers["ETag"] = _bootstrapStyle.etag;
-    response.writeBody(_bootstrapStyle.data, "text/css");
-  }
-
-  void bootstrapJs(HTTPServerRequest req, HTTPServerResponse response) {
-    if("If-None-Match" in req.headers && req.headers["If-None-Match"] == _bootstrapJs.etag) {
-      response.statusCode = 304;
-      response.writeVoidBody;
-      return;
-    }
-
-    response.headers["Cache-Control"] = "max-age=3600";
-    response.headers["ETag"] = _bootstrapJs.etag;
-    response.writeBody(_bootstrapJs.data, "text/javascript");
-  }
-
-  void jquery(HTTPServerRequest req, HTTPServerResponse response) {
-    if("If-None-Match" in req.headers && req.headers["If-None-Match"] == _jquery.etag) {
-      response.statusCode = 304;
-      response.writeVoidBody;
-      return;
-    }
-
-    response.headers["Cache-Control"] = "max-age=3600";
-    response.headers["ETag"] = _jquery.etag;
-    response.writeBody(_jquery.data, "text/javascript");
   }
 }
 
