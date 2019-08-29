6a01d07090e9e86abfac0b0c84c81e0a3d710773
Szabo Bogdan <szabobogdan@yahoo.com>
2019-08-29 19:33:54 +0200

    add ember permisive auth

--- source/vibeauth/router/ember.d
+++ source/vibeauth/router/ember.d
@@ -18,40 +18,61 @@ class EmberSimpleAuth : BaseAuthRouter {
     super(userCollection);
   }
 
+  void updateContext(HTTPServerRequest req, HTTPServerResponse res, string bearer) {
+    User user;
+
+    try {
+      user = collection.byToken(bearer);
+
+      req.username = user.id;
+      req.context["email"] = user.email;
+    } catch(Exception) {
+      respondUnauthorized(res);
+    }
+  }
+
   override {
     void mandatoryAuth(HTTPServerRequest req, HTTPServerResponse res) {
-      if("ember_simple_auth-session" !in req.cookies || "User-Agent" !in req.headers) {
+      if(!req.hasValidEmberSession) {
         respondUnauthorized(res);
         return;
       }
 
-      Json data;
+      Json data = req.sessionData;
 
-      try {
-        data = req.cookies["ember_simple_auth-session"].parseJsonString;
-      } catch(Exception) {
-        respondUnauthorized(res);
-        return;
-      }
-
-      if("authenticated" !in data || "access_token" !in data["authenticated"]) {
+      if(data.type != Json.Type.object || "authenticated" !in data || "access_token" !in data["authenticated"]) {
         respondUnauthorized(res);
         return;
       }
 
       string bearer = data["authenticated"]["access_token"].to!string;
 
-      User user;
-      try {
-        user = collection.byToken(bearer);
-      } catch(Exception) {
-        respondUnauthorized(res);
-        return;
-      }
+      updateContext(req, res, bearer);
     }
 
     void permisiveAuth(HTTPServerRequest req, HTTPServerResponse res) {
+      if("ember_simple_auth-session" !in req.cookies && "User-Agent" !in req.headers) {
+        return;
+      }
 
+      if(!req.hasValidEmberSession) {
+        respondUnauthorized(res);
+        return;
+      }
+
+      Json data = req.sessionData;
+
+      if(data.type != Json.Type.object) {
+        respondUnauthorized(res);
+        return;
+      }
+
+      if("authenticated" !in data || "access_token" !in data["authenticated"]) {
+        return;
+      }
+
+      string bearer = data["authenticated"]["access_token"].to!string;
+      updateContext(req, res, bearer);
     }
   }
 }
@@ -148,7 +169,7 @@ unittest {
     .end;
 }
 
-/// with mandatory auth it should return 401 on missing authenticated
+/// with mandatory auth it should return 401 on missing authenticated data
 unittest {
   auto router = testRouter;
 
@@ -170,4 +191,107 @@ unittest {
     .header("Cookie", "ember_simple_auth-session=authenticated%22%3A%7B%7D%7D")
     .expectStatusCode(401)
     .end;
+}
+
+/// with mandatory auth it should set the email on valid credentials
+unittest {
+  testRouter
+    .request.get("/email")
+    .header("User-Agent", "something")
+    .header("Cookie", "ember_simple_auth-session=%7B%22authenticated%22%3A%7B%22access_token%22%3A%22" ~ bearerToken.name ~ "%22%7D%7D")
+    .expectStatusCode(200)
+    .end((Response response) => {
+      response.bodyString.should.equal("user@gmail.com");
+    });
+}
+
+/// with permisive auth it should return 200 on missing cookie or useragent
+unittest {
+  testRouter(false).request.get("/sites").expectStatusCode(200).end();
+}
+
+/// with permisive auth it should return 401 on invalid json
+unittest {
+  testRouter(false)
+    .request.get("/sites")
+    .header("User-Agent", "something")
+    .header("Cookie", "ember_simple_auth-session=authenticated%22%3A%7B%7D%7D")
+    .expectStatusCode(401)
+    .end;
+}
+
+/// with permisive auth it should return 401 on invalid credentials
+unittest {
+  testRouter(false)
+    .request.get("/sites")
+    .header("User-Agent", "something")
+    .header("Cookie", "ember_simple_auth-session=%7B%22authenticated%22%3A%7B%22access_token%22%3A%22%22%7D%7D")
+    .expectStatusCode(401)
+    .end;
+}
+
+/// with permisive auth it should return 401 on missing user agent
+unittest {
+  testRouter(false)
+    .request.get("/sites")
+    .header("Cookie", "ember_simple_auth-session=%7B%22authenticated%22%3A%7B%22access_token%22%3A%22" ~ bearerToken.name ~ "%22%7D%7D")
+    .expectStatusCode(401)
+    .end;
+}
+
+/// with permisive auth it should return 401 on missing ember_simple_auth-session cookie
+unittest {
+  testRouter(false)
+    .request.get("/sites")
+    .header("User-Agent", "something")
+    .expectStatusCode(401)
+    .end;
+}
+
+/// with permisive auth it should return 200 on valid credentials
+unittest {
+  testRouter(false)
+    .request.get("/sites")
+    .header("User-Agent", "something")
+    .header("Cookie", "ember_simple_auth-session=%7B%22authenticated%22%3A%7B%22access_token%22%3A%22" ~ bearerToken.name ~ "%22%7D%7D")
+    .expectStatusCode(200)
+    .end;
+}
+
+/// with permisive auth it should return 200 on missing authenticated keys
+unittest {
+  testRouter(false)
+    .request.get("/sites")
+    .header("User-Agent", "something")
+    .header("Cookie", "ember_simple_auth-session=%7B%22authenticated%22%3A%7B%7D%7D")
+    .expectStatusCode(200)
+    .end;
+}
+
+/// with permisive auth it should set the email on valid credentials
+unittest {
+  testRouter(false)
+    .request.get("/email")
+    .header("User-Agent", "something")
+    .header("Cookie", "ember_simple_auth-session=%7B%22authenticated%22%3A%7B%22access_token%22%3A%22" ~ bearerToken.name ~ "%22%7D%7D")
+    .expectStatusCode(200)
+    .end((Response response) => {
+      response.bodyString.should.equal("user@gmail.com");
+    });
+}
+
+bool hasValidEmberSession(HTTPServerRequest req) {
+  return "ember_simple_auth-session" in req.cookies && "User-Agent" in req.headers;
+}
+
+Json sessionData(HTTPServerRequest req) {
+  Json data = Json.emptyObject;
+
+  try {
+    data = req.cookies["ember_simple_auth-session"].parseJsonString;
+  } catch(Exception) {
+    return Json();
+  }
+
+  return data;
 }
